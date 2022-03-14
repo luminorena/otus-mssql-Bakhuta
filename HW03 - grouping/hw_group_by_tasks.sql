86f0a4c15689a3b99cb80538f77d5e3319d02a84
bakhuta.oa@gmail.com <grd8rpxfwr6pa3>
2022-03-14 23:25:30 +0300

    HW-03

---
+++ HW03 - grouping/hw_group_by_tasks.sql
@@ -0,0 +1,115 @@
+/*
+Домашнее задание по курсу MS SQL Server Developer в OTUS.
+Занятие "02 - Оператор SELECT и простые фильтры, GROUP BY, HAVING".
+
+Задания выполняются с использованием базы данных WideWorldImporters.
+
+Бэкап БД можно скачать отсюда:
+https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
+Нужен WideWorldImporters-Full.bak
+
+Описание WideWorldImporters от Microsoft:
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-what-is
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-oltp-database-catalog
+*/
+
+-- ---------------------------------------------------------------------------
+-- Задание - написать выборки для получения указанных ниже данных.
+-- ---------------------------------------------------------------------------
+
+USE WideWorldImporters
+
+/*
+1. Посчитать среднюю цену товара, общую сумму продажи по месяцам
+Вывести:
+* Год продажи (например, 2015)
+* Месяц продажи (например, 4)
+* Средняя цена за месяц по всем товарам
+* Общая сумма продаж за месяц
+
+Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
+*/
+
+select
+		grouping (year(InvoiceDate)) as grouping_year
+	   ,grouping (month(InvoiceDate)) as grouping_month
+	   ,year(InvoiceDate) as [year]
+	   ,month(InvoiceDate) as [month]
+	   ,avg(UnitPrice) as avgPrice 
+	   from Sales.Invoices si
+join sales.OrderLines sol on si.OrderID = sol.OrderID
+group by rollup (year(InvoiceDate), month(InvoiceDate)), UnitPrice
+order by  [month] desc 
+
+/*
+2. Отобразить все месяцы, где общая сумма продаж превысила 10 000
+
+Вывести:
+* Год продажи (например, 2015)
+* Месяц продажи (например, 4)
+* Общая сумма продаж
+
+Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
+*/
+
+select	year(InvoiceDate) as [year]
+	   ,month(InvoiceDate) as [month]
+	   ,sum(UnitPrice * sol.Quantity) as sumPrice 
+	   from Sales.Invoices si
+join sales.OrderLines sol on si.OrderID = sol.OrderID
+group by month(InvoiceDate), year(InvoiceDate)
+having sum(UnitPrice * sol.Quantity) > 10000
+
+
+/*
+3. Вывести сумму продаж, дату первой продажи
+и количество проданного по месяцам, по товарам,
+продажи которых менее 50 ед в месяц.
+Группировка должна быть по году,  месяцу, товару.
+
+Вывести:
+* Год продажи
+* Месяц продажи
+* Наименование товара
+* Сумма продаж
+* Дата первой продажи
+* Количество проданного
+
+Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
+*/
+
+select year(so.OrderDate) as [year], month(so.OrderDate) as [month], sol.[Description]
+, sum(sol.PickedQuantity * sol.UnitPrice) as [sum], si.InvoiceDate
+,sol.PickedQuantity
+from sales.Invoices si
+join sales.Orders so on si.OrderID = so.OrderID
+join sales.OrderLines sol on sol.OrderID = si.OrderID
+group by year(so.OrderDate), month(so.OrderDate), 
+sol.[Description], sol.PickedQuantity, si.InvoiceDate
+having sum(sol.PickedQuantity * sol.UnitPrice) < 50
+
+-- ---------------------------------------------------------------------------
+-- Опционально
+-- ---------------------------------------------------------------------------
+/*
+Написать запросы 2-3 так, чтобы если в каком-то месяце не было продаж,
+то этот месяц также отображался бы в результатах, но там были нули.
+*/
+
+/*
+можно ли использовать подзапросы и CTE? Тем на это не было.
+Есть один предполагаемый вариант, как можно
+сделать эту задачу - через cross join sys.objects, то есть получить массив чисел от 1 до 12,
+а потом уже нули определить через case when.
+Ну или через 
+select 1 as '1'
+union
+select 2 as '2'
+получить массив чисел от 1 до 12, но это велосипед, можно по-нормальному сделать как-то
+И вопрос, если нет продаж, то нет месяца в InvoiceDate - тут как-то надо условие составлять.
+Тут я запуталась, как реализовать.
+*/
+
+
+
+
